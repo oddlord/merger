719693868f38fc6b092c61a215ffae9ff354ef0a
Tommaso Papini <tommy39@gmail.com>
2018-01-19 21:47:12 +0100

    ensure_dir()

--- merger.py
+++ merger.py
@@ -1,3 +1,6 @@
+import git
+import os
+from subprocess import call
 import sys
 
 def config_error():
@@ -10,11 +13,19 @@ try:
 except ImportError:
     config_error()
 
-def main():
+home_dir = os.path.expanduser('~') + '/'
+base_dir = home_dir + 'test/'
 
+def ensure_dir(dir_path):
+    directory = os.path.dirname(dir_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def main():
     if main_repo is None or sub_repos is None:
         config_error()
-    print 'lol'
+
+    ensure_dir(base_dir)
 
 if __name__ == '__main__':
     main()
